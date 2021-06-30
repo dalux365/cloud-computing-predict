ab0b038ce6b741d4bce2bfc277727d19a2a92003
dalux365 <70945670+dalux365@users.noreply.github.com>
2021-06-30 06:08:39 +0200

    pics ans url

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://main.d3h9kqyqmlcng0.amplifyapp.com",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
