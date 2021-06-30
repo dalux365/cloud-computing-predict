330d084bd2ba7aa9ab46b232eae8c83684077707
dalux365 <70945670+dalux365@users.noreply.github.com>
2021-06-30 05:52:10 +0200

    added api/posting

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://0ea417spz7.execute-api.eu-west-1.amazonaws.com/posting",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
