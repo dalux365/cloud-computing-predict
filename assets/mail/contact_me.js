73908e6241878af84227fc70d21fb4905b4b4adf
Dalux360 <47083991+Dalux360@users.noreply.github.com>
2021-07-01 19:34:01 +0200

    pi

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://0ea417spz7.execute-api.eu-west-1.amazonaws.com/posting",
+                url: "https://0ea417spz7.execute-api.eu-west-1.amazonaws.com",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
